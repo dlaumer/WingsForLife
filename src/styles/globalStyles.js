7a937da0ad16106451bbf6b17043aa8b84361411
Daniel Laumer <daniel.laumer@gmail.com>
2025-04-18 02:17:57 +0200

    First commit

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0 !important;
+    font-family: Arial, sans-serif;
+  }
+`;
+
+export default GlobalStyles;
